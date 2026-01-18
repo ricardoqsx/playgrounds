89bfe77a214d0a0e0d06a4fe3fade52e3bc37e24
ricardoqsx <ricardoqs@me.com>
2026-01-17 21:21:39 -0500

    deletinh something to test...

--- src/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .admin_db import *
-from .admin_mdb import *
-from .queries_db import *
-from .users import *
-from .queries_mdb import *
--- src/app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth_bp import *
-from .admin_bp import *
-from .home_bp import *
-from .testing_bp import *
-from .debug_bp import *
-from .users_bp import *
